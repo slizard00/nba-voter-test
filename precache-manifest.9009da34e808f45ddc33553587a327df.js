46ecf50706fbf78b129b97365b217be043fec101
slizard00 <slizard00@users.noreply.github.com>
2020-06-05 16:02:06 -0500

    Updates

---
+++ precache-manifest.9009da34e808f45ddc33553587a327df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c335c054d0244fafb3ff8b75506bf435",
+    "url": "/nba-voter-test/index.html"
+  },
+  {
+    "revision": "3857254d51f126900256",
+    "url": "/nba-voter-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0c1542a42e02464fe3bf",
+    "url": "/nba-voter-test/static/js/2.23ed0ba0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nba-voter-test/static/js/2.23ed0ba0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3857254d51f126900256",
+    "url": "/nba-voter-test/static/js/main.5d85f6ce.chunk.js"
+  },
+  {
+    "revision": "bc7fbf0dbfcbec926837",
+    "url": "/nba-voter-test/static/js/runtime-main.ac380578.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nba-voter-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
